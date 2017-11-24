69b8c81ea7ae1e9307c46119945db8281d4995b6
Hellcrow <bakamakafoqq@gmail.com>
2017-11-24 19:38:25 +0200

    TestCase
    Поправил тест структуру

--- TreeTests.java
+++ TreeTests.java
@@ -2,10 +2,15 @@ import org.junit.Test;
 
 public class TreeTests
 {
-	BSTree tr = new BSTree();
+	TreeInterface tree;
+
+	public TreeTests()
+	{
+		tree = new BSTree();
+	}
 
 	@Test
-	public void test()
+	public void testLeaves()
 	{
 		int init[] =
 		{ 53, 30, 14, 39, 9, 23, 34, 47, 72, 61, 84, 79 };
