bb0cfbed10a75bee1988a54f02353c9d8ada0694
Hellcrow <bakamakafoqq@gmail.com>
2017-11-24 19:28:24 +0200

    24.11.2017
    Новая структура

---
+++ TreeTests.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+public class TreeTests
+{
+	BSTree tr = new BSTree();
+
+	@Test
+	public void test()
+	{
+		int init[] =
+		{ 53, 30, 14, 39, 9, 23, 34, 47, 72, 61, 84, 79 };
+
+	}
+}
